e03f21d888f5ff8997809cfe307471ead74e34e0
emperor-pig <jzhu553@uwo.ca>
2021-08-16 19:59:45 -0400

    Update from judy

--- COVID_Model_1.3/src/Auxiliary.java
+++ COVID_Model_1.3/src/Auxiliary.java
@@ -3,12 +3,8 @@ import java.io.FileReader;
 import java.util.Scanner;
 
 public class Auxiliary{
-    public static void main(String[] args) throws Exception{
-        System.out.println("./COVID_Model.iml");
-        Scanner scan = new Scanner(new File(".\\Model IO\\cases_by_status_and_phu.csv"));
-        while (scan.hasNext()) {
-            System.out.println(scan.nextLine());
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 
 }
--- COVID_Model_1.3/src/Main.java
+++ COVID_Model_1.3/src/Main.java
@@ -5,7 +5,36 @@ public class Main {
 
     /**
      * Mode 0: Work, day time
-     * Mode 1: Resident, night time
+     * Mode 1: ResidJing Yan (Judy) Zhu is inviting you to a scheduled Zoom meeting.
+     *
+     * Topic: Jing Yan (Judy) Zhu's Personal Meeting Room
+     *
+     * Join Zoom Meeting
+     * https://westernuniversity.zoom.us/j/5364147796?pwd=bXVKS2MrallmejliU1RDc25wUHJudz09
+     *
+     * Meeting ID: 536 414 7796
+     * Passcode: PC2Huw
+     * One tap mobile
+     * +16699006833,,5364147796#,,,,*110292# US (San Jose)
+     * +19292056099,,5364147796#,,,,*110292# US (New York)
+     *
+     * Dial by your location
+     *         +1 669 900 6833 US (San Jose)
+     *         +1 929 205 6099 US (New York)
+     * Meeting ID: 536 414 7796
+     * Passcode: 110292
+     * Find your local number: https://westernuniversity.zoom.us/u/aeSfvsZIh
+     *
+     * Join by SIP
+     * 5364147796@zoomcrc.com
+     *
+     * Join by H.323
+     * 162.255.37.11 (US West)
+     * 162.255.36.11 (US East)
+     * 69.174.57.160 (Canada Toronto)
+     * 65.39.152.160 (Canada Vancouver)
+     * Meeting ID: 536 414 7796
+     * Passcode: 110292ent, night time
      */
 
     public static void main(String[] args) {
