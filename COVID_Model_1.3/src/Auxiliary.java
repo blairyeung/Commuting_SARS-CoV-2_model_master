09b4168ce5e129ce8b86569eb9a0aebbbaf5f5f3
blairyeung <53135008+blairyeung@users.noreply.github.com>
2021-08-22 20:37:33 -0400

    Updated Path

--- COVID_Model_1.3/src/Auxiliary.java
+++ COVID_Model_1.3/src/Auxiliary.java
@@ -1,14 +1,19 @@
 import java.io.File;
 import java.io.FileReader;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Auxiliary{
     public static void main(String[] args) throws Exception{
+
         System.out.println("./COVID_Model.iml");
-        Scanner scan = new Scanner(new File(".\\Model IO\\cases_by_status_and_phu.csv"));
+        //Scanner scan = new Scanner(new File(".\\Model IO\\cases_by_status_and_phu.csv"));
+        String path = "COVID_Model_1.3\\COVID_Model.iml";
+        Scanner scan = new Scanner(new File(path));
         while (scan.hasNext()) {
             System.out.println(scan.nextLine());
         }
     }
 
 }
+
