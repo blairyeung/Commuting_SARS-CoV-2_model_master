d092dca4c8ac382d0d8b1f58dd043252b408796d
blairyeung <loveblairsky@gmail.com>
2021-08-23 20:05:56 -0400

    Auxiliry changed

--- COVID_Model_1.3/src/Auxiliary.java
+++ COVID_Model_1.3/src/Auxiliary.java
@@ -5,14 +5,7 @@ import java.util.Scanner;
 
 public class Auxiliary{
     public static void main(String[] args) throws Exception{
-
-        System.out.println("./COVID_Model.iml");
-        //Scanner scan = new Scanner(new File(".\\Model IO\\cases_by_status_and_phu.csv"));
-        String path = "COVID_Model_1.3\\COVID_Model.iml";
-        Scanner scan = new Scanner(new File(path));
-        while (scan.hasNext()) {
-            System.out.println(scan.nextLine());
-        }
+        System.out.println("Hi");
     }
 
 }
